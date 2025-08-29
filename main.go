2dfd3f653d28bb80337fa050f0117d2c19062467
batuhan <batuhan@192.168.1.41>
2025-08-29 11:51:16 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/home.html")
+}
+
+func coursePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/courses.html")
+}
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/about.html")
+}
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/contact.html")
+}
+
+func main() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/courses", coursePage)
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/contact", contactPage)
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
